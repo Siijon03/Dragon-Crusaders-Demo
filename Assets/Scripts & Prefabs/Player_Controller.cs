3650a26926675ff9642c9fca249cb2884ab6bf3f
Siijon03 <117677593+Siijon03@users.noreply.github.com>
2022-12-01 16:48:07 +0000

    Dragon Crusaders Demo V0.1.5
    Fixed Movement, based on grid size and grid position. Player now will always spawn in the middle of the grid.
    
    Next Step: Add working buttons & GUI. Note: Use Debug to make sure the buttons work

--- Assets/Scripts & Prefabs/Player_Controller.cs
+++ Assets/Scripts & Prefabs/Player_Controller.cs
@@ -9,13 +9,22 @@ public class Player_Controller : MonoBehaviour
     public float x = 0f;
     public float y = 0f;
 
+    //This stores the coordinates 
+    //By making this public, we can send it to start and then pass it to public.
+    public Vector3 Start_Position = new Vector3(0,0,0);
+
+    //This stores the player's co-ordinates 
+    public Vector2 Player_Position = new Vector2(0,0);
+
     // Start is called before the first frame update
     void Start()
     {
         try
         {
+            //This accesses our grid manager script and Creates a new variable 
+            GameObject GridObject = GameObject.Find("GridManager");
             //This accesses our grid manager script and calls upon those functions
-            Grid_Manager GridScript = GameObject.Find("GridManager").GetComponent<Grid_Manager>();
+            Grid_Manager GridScript = GridObject.GetComponent<Grid_Manager>();
             //Gets the values of the rows and columns 
              y = GridScript.Grid_Row;
              x = GridScript.Grid_Columns;
@@ -27,8 +36,11 @@ public class Player_Controller : MonoBehaviour
             float new_x = ((float)(Math.Ceiling(x / 2)));
 
             //This makes it so the player will spawn in the middle by adjusting those co-ordinates 
-            gameObject.transform.position = new Vector2(new_x - 1 , (new_y * - 1) + 1);
-            
+            gameObject.transform.position = new Vector2(GridObject.transform.position.x + (new_x - 1) , GridObject.transform.position.y + ((new_y * - 1) + 1));
+            //Store player position as local co-ordinates
+            Player_Position = new Vector2(new_x, new_y);
+            //This stores the starting position. 
+            Start_Position = gameObject.transform.position;
         }
         //Just in case the code cannot find the grid 
         catch (Exception e)
@@ -44,35 +56,40 @@ public class Player_Controller : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             //This is so the player can move around without going too far up.
-            if (!(gameObject.transform.position.y == 0)){
+            if (!(Player_Position.y == 1))
+            {
                 gameObject.transform.position += new Vector3(0f, +1f);
+                Player_Position += new Vector2(0f, -1f);
             }
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             //This is so the player can move around without going too far down.
-            if (!(gameObject.transform.position.y == (y * -1)+ 1))
+            if (!(Player_Position.y == y))
             {
                 gameObject.transform.position += new Vector3(0f, -1f);
+                Player_Position += new Vector2(0f, +1f);
             }
         }
 
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             //This is so the player can move around without going too far right.
-            if (!(gameObject.transform.position.x == x - 1))
+            if (!(Player_Position.x == x))
             {
                 gameObject.transform.position += new Vector3(+1f, 0f);
+                Player_Position += new Vector2(+1f, 0f);
             }
         }
 
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             //This is so the player can move around without going too far left.
-            if (!(gameObject.transform.position.x == 0))
+            if (!(Player_Position.x == 1))
             {
                 gameObject.transform.position += new Vector3(-1f, 0f);
+                Player_Position += new Vector2(-1f, 0f);
             }
         }
     }
